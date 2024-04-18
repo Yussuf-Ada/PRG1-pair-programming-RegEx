dea350aea0784dd666367309c2c4cf214c4e7630
Yussuf-Ada <167077054+Yussuf-Ada@users.noreply.github.com>
2024-04-18 14:01:48 +0000

    initial function

--- regex.js
+++ regex.js
@@ -10,6 +10,26 @@ console.log(validWholeNumberRegEx.test(456))
 // Create three functions which check user input for three pieces of data.
 // 1. Their name; 2. Their age; 3. Their height. 
 
+const readline = require("readline-sync");
+
+function passwordChecker() {
+  let passwordConditions =
+    /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*()_+{}\[\]:;<>,.?~\\/\|=]).{8,}$/;
+  let userPass = readline.question("Please enter a strong password \n");
+  while (true) {
+    if (passwordConditions.test(userPass)) {
+      console.log("Great your password has been accepted ");
+      break;
+    } else {
+      console.log(
+        "Your password must be:\n - longer than 8 characters\n - include a uppercase letter\n - include a lowercase letter\n - include a number "
+      );
+      userPass = readline.question("Please enter a strong password \n");
+    }
+  }
+}
+
+passwordChecker();
 
 
 
